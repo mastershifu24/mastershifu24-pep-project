79936fa98de89629b698b2ab227d57c41733d04c
Ahmed Shifa <ahmedshifa298@gmail.com>
2023-09-03 10:24:08 +0000

    uodates to Controller

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -51,6 +51,137 @@ public class SocialMediaController {
         context.json("sample text");
     }
 
+     // create account
+     private void addAccountHandler(Context ctx) {
+        
+        // get request information
+        Account account = ctx.bodyAsClass(Account.class);
+        // call service method
+        Account accountAdded = accountService.addAccount(account);
+        // send result to client
+        if(accountAdded != null) {
+            ctx.json(accountAdded);
+        } else {
+            ctx.status(400);
+        }
+    }
+
+    // login to account
+    private void loginAccountHandler(Context ctx) {
+        // get request information
+        Account account = ctx.bodyAsClass(Account.class);
+        // call service method
+        Account accountLogin = accountService.getAccount(account.getUsername(), account.getPassword());
+        // send result to client
+        if(accountLogin != null) {
+            ctx.json(accountLogin);
+        } else {
+            ctx.status(401);
+        }
+
+    }
+
+    // get account by id
+    private void getAccountByIdHandler(Context ctx) {
+        // get request information
+        String idString = ctx.pathParam("account_id");
+        int id = Integer.parseInt(idString);
+        // call service method
+        Account account = accountService.getAccountById(id);
+        // send result to client
+        if(account != null) {
+            ctx.json(account);
+        } else {
+            ctx.status(400);
+        }
+    }
+
+    // get all accounts
+    private void getAllAccountsHandler(Context ctx) {
+        // call service method
+        ArrayList<Account> accounts = accountService.getAllAccounts();
+        // send result to client
+        if(accounts != null) {
+            ctx.json(accounts);
+        } else {
+            ctx.status(400);
+        }
+    }
+
+    // create message
+    private void addMessageHandler(Context ctx) {
+        // get request information
+        Message message = ctx.bodyAsClass(Message.class);
+        // call service method
+        Message messageAdded = messageService.addMessage(message);
+        // send result to client
+        if (messageAdded != null) {
+            ctx.json(messageAdded);
+        } else {
+            ctx.status(400);
+        }
+    }
+
+    // get all messages
+    private void getAllMessagesHandler(Context ctx) {
+        // call service method
+        ArrayList<Message> messages = messageService.getAllMessages();
+        // send result to client
+        if(messages != null) {
+            ctx.json(messages);
+        } else {
+            ctx.status(500);
+        }
+    }
+
+    // get message by id
+    private void getMessageByIdHandler(Context ctx) {
+        // get request information
+        String idString = ctx.pathParam("message_id");
+        int id = Integer.parseInt(idString);
+        // call service method
+        Message message = messageService.getMessageById(id);
+        // send result to client
+        if(message != null) {
+            ctx.json(message);
+        } else {
+            ctx.status(200);
+            ctx.json("");
+        }
+    }
+
+    // get all user messages
+    private void getAllMessagesByUserHandler(Context ctx) {
+        // get request information
+        String idString = ctx.pathParam("account_id");
+        int id = Integer.parseInt(idString);
+        // call service metod
+        ArrayList<Message> userMessages = messageService.getAllMessagesByUser(id);
+        // send results to client
+        if(userMessages != null) {
+            ctx.json(userMessages);
+        } else {
+            ctx.status(500);
+        }
+    }
+
+    // update message text
+    private void updateMessageHandler(Context ctx) {
+        // get request information
+        Map<String, String> messageMap = ctx.bodyAsClass(Map.class);
+        String messageText = messageMap.get("message_text");
+        String idString = ctx.pathParam("message_id");
+        int id = Integer.parseInt(idString);
+        // call service method
+        Message message = messageService.updateMessage(id, messageText);
+        // send result to client
+        if(message != null) {
+            ctx.json(message);
+        } else {
+            ctx.status(400);
+        }
+    }
+
      // delete message
      private void deleteMessageHandler(Context ctx) {
         // get request information
